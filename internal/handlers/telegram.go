49a094f981ef8a7ffa661ed8252a47992afa811f
gwkeo <raxman.i@mail.ru>
2024-11-14 13:43:38 +0300

    Добавлены поля в Message и раскомментирован код хендлера

--- internal/handlers/telegram.go
+++ internal/handlers/telegram.go
@@ -1,35 +1,49 @@
 package handlers
 
-//import (
-//	"github.com/gwkeo/telegram_favourites_plus/internal/models"
-//	"sync"
-//)
-//
-//type Type int
-//
-//const (
-//	Text Type = iota
-//	Animation
-//)
-//
-//func HandleTelegramResponse(results []models.Result) {
-//	wg := &sync.WaitGroup{}
-//	for _, r := range results {
-//		wg.Add(1)
-//		go func() {
-//			resType := TypeOfResult(r, wg)
-//
-//		}()
-//	}
-//
-//	wg.Wait()
-//}
-//
-//func TypeOfResult(r models.Result, wg *sync.WaitGroup) Type {
-//	defer wg.Done()
-//	if r.Message.Text != "" {
-//		return Text
-//	} else {
-//		return Animation
-//	}
-//}
+import (
+	"github.com/gwkeo/telegram_favourites_plus/internal/models"
+	"sync"
+)
+
+type Type int
+
+const (
+	Text Type = iota
+	Animation
+	Photo
+	Document
+	Video
+	Voice
+	VideoNote
+)
+
+func TypeOfResult(r models.Result, wg *sync.WaitGroup) Type {
+	defer wg.Done()
+	if r.Message.Text != "" {
+		return Text
+	} else if r.Message.Animation != nil {
+		return Animation
+	} else if r.Message.Photo != nil {
+		return Photo
+	} else if r.Message.Document != nil {
+		return Document
+	} else if r.Message.Voice != nil {
+		return Voice
+	} else if r.Message.Video != nil {
+		return Video
+	} else {
+		return VideoNote
+	}
+}
+
+func HandleTelegramResponse(results []models.Result) {
+	wg := &sync.WaitGroup{}
+	for _, r := range results {
+		wg.Add(1)
+		go func() {
+			resType := TypeOfResult(r, wg)
+		}()
+	}
+
+	wg.Wait()
+}
--- internal/models/response.go
+++ internal/models/response.go
@@ -11,15 +11,18 @@ type Result struct {
 }
 
 type Message struct {
-	Id        int      `json:"message_id"`
-	Chat      Chat     `json:"chat"`
-	Text      string   `json:"text,omitempty"`
-	Animation struct{} `json:"animation,omitempty"`
+	Id        int       `json:"message_id"`
+	Chat      Chat      `json:"chat"`
+	Text      string    `json:"text,omitempty"`
+	Animation *struct{} `json:"animation,omitempty"`
+	Photo     *struct{} `json:"photo,omitempty"`
+	Document  *struct{} `json:"document,omitempty"`
+	Video     *struct{} `json:"video,omitempty"`
+	Voice     *struct{} `json:"voice,omitempty"`
+	VideoNote *struct{} `json:"video_note,omitempty"`
+	Caption   string    `json:"caption,omitempty"`
 }
 
 type Chat struct {
 	Id int `json:"id"`
-}
-
-type Animation struct {
 }
